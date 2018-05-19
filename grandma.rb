5590e486d03d807ff296ab9745588c9e5a8346bd
Leticia Molina <letimolina@gmail.com>
2018-05-19 21:02:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,5 +14,10 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif 
+  elsif !phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+end
     
